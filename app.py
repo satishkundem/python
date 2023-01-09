61ab88b2e0f53181cf99a7131cff081029ff89e5
Ram satish kundem <112755398+satishkundem@users.noreply.github.com>
2023-01-09 19:25:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+user_input = 'random'
+data = []
+
+def show_menu():
+    print('Menu: ')
+    print('1, Add an item')
+    print('2, Mark as done')
+    print('3, View an item')
+    print('4, Exit')
+
+while user_input != '4':
+    
+    show_menu()
+    user_input = input('Enter your choice: ')
+    
+    if user_input == '1':
+        item = input('What is to br done? ')
+        data.append(item)
+        print('Added item', item)
+    elif user_input == '2':
+        item = input('What is to be done? ')
+        if item in data:
+            data.remove(item)
+            print('Removed item:', item)
+    elif user_input == '3':
+        print('List of to-do list:')
+        for item in data:
+            print(item)
+    elif user_input == '4':
+        print('Goodbye')
+    else:
+        print('please enter one of 1, 2, 3 or 4')                
+
